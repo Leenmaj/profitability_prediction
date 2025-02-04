2ce98c155d633dc60651489eb88c18c77d1ede41
haifa-mk <145285442+haifa-mk@users.noreply.github.com>
2025-02-04 19:10:05 +0300

    file for preprocessing function to import them

---
+++ preprocessing.py
@@ -0,0 +1,42 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import torch
+import pandas as pd
+import numpy as np
+from imblearn.over_sampling import SMOTE
+
+
+
+def preprocess_data(file_path):
+    # Load dataset
+    df = pd.read_csv(file_path)
+
+    # Handle missing values (drop rows with missing data)
+    df = df.dropna()
+
+    # Encode categorical columns
+    categorical_cols = df.select_dtypes(include=['object']).columns
+    label_encoders = {}
+    for col in categorical_cols:
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+        label_encoders[col] = le
+
+    # Separate features and labels
+    X = df.drop("target", axis=1)
+    y = df["target"]
+
+    # Normalize numerical features
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    # Split into train and test sets
+    n_samples , n_features = X.shape
+    n_channel = n_features
+    n_length = 1
+
+    X = X.reshape(n_samples, n_channel, n_length)
+
+
+
+    return X ,y
